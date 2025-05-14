054a198c31db4617f635609a90221ed17b4a7664
Nghiabe <nghiantk.21it@vku.udn.vn>
2025-05-14 08:18:16 +0700

    hsxsadded

--- routes/web.php
+++ routes/web.php
@@ -125,6 +125,7 @@ Route::get('/{id}', [App\Http\Controllers\PageController::class, 'getsanpham'])-
 
 // Route cho việc thêm sản phẩm vào giỏ hàng (addtocart)
 // Controller
+Route::post('/addtocart/{id}', [App\Http\Controllers\PageController::class, 'getaddtocart']);
 Route::post('/sanpham/addtocart/{id}', [PageController::class, 'getaddtocart'])->name('addtocart');
 Route::get('/sanpham/addtocart/{id}', [App\Http\Controllers\PageController::class, 'getaddtocart'])->name('addtocart');
 // Route cho việc xóa sản phẩm khỏi giỏ hàng (delete-cart)
